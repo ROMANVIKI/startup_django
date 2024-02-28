a00c4b12617efd7a75f006c6819db4a3aab4e383
ROMANVIKI <vikiadhi2016@gmail.com>
2024-02-28 21:54:20 +0530

    product-update

--- startup/views.py
+++ startup/views.py
@@ -235,12 +235,7 @@ def product_detail(request, id, slug):
     return render(request, 'detail.html', {'product': product})
 
 
-def payment_completed(request):
-    return render(request, 'completed.html')
 
-
-def payment_canceled(request):
-    return render(request, 'canceled.html')
 
 
 
