63354e718925b741f301b19b0c144173c584f0a6
ROMANVIKI <vikiadhi2016@gmail.com>
2024-02-29 10:32:47 +0530

    endpoints refactoring

--- startup/urls.py
+++ startup/urls.py
@@ -5,22 +5,22 @@ from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("about/", views.About.as_view(), name="about"),
-    path("contact/", views.Contact.as_view(), name="contact"),
+    path("about", views.About.as_view(), name="about"),
+    path("contact", views.Contact.as_view(), name="contact"),
 
    
-    path("signin/", views.signin, name="signin"),
-    path("logout/", views.logout_view, name="logout"),
+    path("signin", views.signin, name="signin"),
+    path("logout", views.logout_view, name="logout"),
 
-    path("signup/", views.signup, name="signup"),
-    path("dashboard/", views.dashborad, name="dashboard"),
+    path("signup", views.signup, name="signup"),
+    path("dashboard", views.dashborad, name="dashboard"),
 
 
 
-    path('get_username/', views.get_username, name='get-username'),
-    path('product-list/', views.ProductListView.as_view(), name='product-list'),
+    path('get_username', views.get_username, name='get-username'),
+    path('product-list', views.ProductListView.as_view(), name='product-list'),
     path('product-detail/<int:id>/<slug:slug>/', views.product_detail, name='product-detail'),
-    path('payment-process/', views.payment_process, name='payment-process'),
+    path('payment-process', views.payment_process, name='payment-process'),
 
 
     
