4d3778f46076fc359f2b27b62c28d248a0296ced
ROMANVIKI <vikiadhi2016@gmail.com>
2024-03-28 16:59:49 +0530

    mar-27

--- project/settings.py
+++ project/settings.py
@@ -28,9 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get('DEBUG')
-
-DEBUG = False
+DEBUG = os.environ.get('DEBUG')
 
 
 ALLOWED_HOSTS = ['www.tokaz.in', 'tokaz.in', 'tokaz.onrender.com']
