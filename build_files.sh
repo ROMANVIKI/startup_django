8d953c7416fb1dbb05d28b7b781fab2553616f1f
ROMANVIKI <vikiadhi2016@gmail.com>
2024-08-30 20:37:05 +0530

    new commit ver

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Ensure the script is executable with `chmod +x build_files.sh`
 echo "Building the project..."
-python3.9 manage.py collectstatic --noinput
+python3.10 manage.py collectstatic --noinput
 echo "Static files collected."
